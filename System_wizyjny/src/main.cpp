d5edc87280aa0be4d1fca268be270890948d8e89
piotr_balon_278314 <278314@student.pwr.edu.pl>
2026-07-21 17:15:32 +0200

    Uporządkowanie kodu

--- System_wizyjny/src/main.cpp
+++ System_wizyjny/src/main.cpp
@@ -2,7 +2,6 @@
 #include "ImgManip.hh"
 #include "driver/uart.h"
 
-// Definicja struktur z jawnym upakowaniem bajtów (packed)
 #pragma pack(push, 1)
 typedef struct {
   uint8_t posX;
@@ -16,38 +15,33 @@ typedef struct {
 } DataFrame;
 #pragma pack(pop)
 
-// Inicjalizacja obiektów kamery i przetwarzania obrazu
 OV2640 Camera;
 ImgManip Manipulator;
 
-// Element strukturalny do operacji morfologicznych
 bool structElem[9] = {1,1,1,1,1,1,1,1,1};
 
-// Prototypy funkcji
 uint32_t measureTime();
 void sendFrame(uint8_t* data, size_t len, uint32_t interval, float fps, uint8_t labels, std::vector<Blob> labels_info);
 void sendInfo(uint8_t labels, std::vector<Blob> labels_info);
 
 void setup() {
-  // Wyłączenie logów systemowych ESP, żeby nie śmieciły na portach
   esp_log_level_set("*", ESP_LOG_NONE);
 
-  // Serial0 (USB do PC) - odpinamy od domyślnych pinów TX/RX, aby zwolnić je dla Serial2
   Serial.begin(115200);
   Serial.flush();
   uart_set_pin(UART_NUM_0, -1, -1, -1, -1);
 
-  // Serial2 - wysyłanie pełnego obrazu do PC (Baudrate 1M, RX=pin 3, TX=pin 1)
   Serial2.setTxBufferSize(26000);
   Serial2.begin(1000000, SERIAL_8N1, 3, 1);
   while(!Serial2) delay(10);
 
-  // Serial1 - komunikacja z STM32 (Baudrate 115200, RX=pin 33, TX=pin 32)
-  // WAŻNE: Połącz pin 32 (TX) z pinem RX w STM32 (PA10)
-  Serial1.begin(115200, SERIAL_8N1, 33, 32); 
+  Serial1.begin(115200, SERIAL_8N1, 14, 13); 
+  Serial1.setTxBufferSize(256);
   while(!Serial1) delay(10);
 
-  // Inicjalizacja peryferiów kamery i algorytmów
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+
   Camera.init();
   Manipulator.init();
 }
@@ -56,7 +50,6 @@ void loop() {
   uint32_t interval = measureTime();
   float fps = 1000.0 / (float)interval;
 
-  // Przechwycenie obrazu z matrycy
   uint8_t * Img = Camera.capture();
 
   Manipulator.detectEdges();
@@ -64,32 +57,19 @@ void loop() {
   Manipulator.separateChannels();
   Manipulator.detectColors(0,180,150,255);
 
-  // Operacje morfologiczne: Zamknięcie -> Otwarcie -> Zamknięcie -> Otwarcie
   Manipulator.dilation(structElem);
   Manipulator.erosion(structElem);
   Manipulator.erosion(structElem);
   Manipulator.dilation(structElem);
-  // Manipulator.dilation(structElem);
-  // Manipulator.erosion(structElem);
-  // Manipulator.erosion(structElem);
-  // Manipulator.dilation(structElem);
 
-  // Segmentacja i analiza obiektów (Blobów)
   uint8_t labels = Manipulator.label();
   Manipulator.countArea();
   Manipulator.findCenter();
   Manipulator.countRoundness();
   Manipulator.checkBottomEdge();
-  // Manipulator.filterEdges();
-
-  // Manipulator.distanceTransform();
-  // Manipulator.separateOverlaps();
-
-  // labels = Manipulator.getLabelsNum();
 
   std::vector<Blob> labels_info = Manipulator.getLabelsInfo();
 
-  // 1. Wysyłanie pełnej klatki obrazu do PC przez Serial2 (jeśli bufor jest wolny)
   if (Serial2.availableForWrite() >= Camera.getWidth()*Camera.getHeight() + 30) 
   {
     sendFrame(
@@ -102,18 +82,14 @@ void loop() {
     );
   }
 
-  // 2. Wysyłanie spakowanych danych o obiektach do STM32 przez Serial1
-  if (Serial1.availableForWrite() >= sizeof(DataFrame) /*millis()%1000 == 0*/) 
+  if (Serial1.availableForWrite() >= sizeof(DataFrame)) 
   {
     sendInfo(labels, labels_info);
   }
 
-  // POPRAWKA: Hamulec bezpieczeństwa. Daje linii UART 50ms ciszy. 
-  // Bez tego STM32 nigdy nie wykryje przerwania IDLE i ignoruje ramki.
   delay(50); 
 }
 
-// Funkcja mierząca czas wykonania pętli
 uint32_t measureTime() {
   static uint32_t last_call_time = 0;
   uint32_t current_time = millis();
@@ -125,7 +101,6 @@ uint32_t measureTime() {
   return interval;
 }
 
-// Funkcja wysyłająca obraz (PC)
 void sendFrame(uint8_t* data, size_t len, uint32_t interval, float fps, uint8_t labels, std::vector<Blob> labels_info) {
   const uint8_t header[] = {0xDE, 0xAD, 0xBE, 0xEF};
   
@@ -144,9 +119,6 @@ void sendFrame(uint8_t* data, size_t len, uint32_t interval, float fps, uint8_t 
     Serial2.write((uint8_t*)&b.x, 4);
     Serial2.write((uint8_t*)&b.y, 4);
     Serial2.write((uint8_t*)&b.roundness, 4);
-
-    // float val = Manipulator.getMatU()[(int)b.y * 176 + (int)b.x];
-    // Serial2.write((uint8_t*)&val, 4);
   }
 
   Serial2.write((uint8_t*)&len, 4);
@@ -156,6 +128,10 @@ void sendFrame(uint8_t* data, size_t len, uint32_t interval, float fps, uint8_t 
 
 void sendInfo(uint8_t labels, std::vector<Blob> labels_info)
 {
+  static bool ledState = false;
+  ledState = !ledState;
+  digitalWrite(2, ledState);
+
   DataFrame m;
 
   m.objNum = labels;
@@ -168,13 +144,15 @@ void sendInfo(uint8_t labels, std::vector<Blob> labels_info)
     m.objList[i].area = 0;
   }
 
+  uint32_t test = 30000;
   
   int idx = 0;
   for(Blob b : labels_info)
   {
     m.objList[idx].posX = (uint8_t)b.x;
     m.objList[idx].posY = (uint8_t)b.y;
-    m.objList[idx].area = (uint32_t)b.area;
+    if(b.bottomEdge) m.objList[idx].area = test;
+    else m.objList[idx].area = (uint32_t)b.area;
 
     idx++;
     if(idx >= 10) break;
